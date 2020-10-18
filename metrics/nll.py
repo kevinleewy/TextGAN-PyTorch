c9119362f2daa51ee6b1398ca9744c8029a02424
Kevin Lee <kevinleewy@hotmail.com>
2020-10-17 23:14:38 -0700

    d

--- metrics/nll.py
+++ metrics/nll.py
@@ -31,8 +31,6 @@ class NLL(Metrics):
         if not self.if_use:
             return 0
 
-        print(next(iter(self.data_loader)))
-
         assert self.model and self.data_loader, 'Need to reset() before get_score()! {} {} {} {} {}'.format(self.name, self.data_loader, not not self.data_loader, self.label_i, len(self.data_loader))
 
         if self.leak_dis is not None:  # For LeakGAN
